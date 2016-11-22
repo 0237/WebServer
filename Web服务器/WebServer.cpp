fd67cfb14f62b9248ce6f2b171f5fd4be08c959d
MYoung <xd_yang@outlook.com>
2016-11-22 21:58:50 +0800

    添加项目文件。

---
+++ Web服务器/WebServer.cpp
@@ -0,0 +1,221 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<winsock2.h>
+#include<iostream>
+#include<string>
+#include<io.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+// 获取文件类型  
+void GetFileType(char * filename, char * fileType)
+{
+	memset(fileType, 0, 20);
+	if (strstr(filename, ".html") || 0 == strcmp(filename, "/"))
+	{
+		strcpy(fileType, "text/html");
+	}
+	else if (strstr(filename, ".bmp"))
+	{
+		strcpy(fileType, "application/x-bmp");
+	}
+	else if (strstr(filename, ".gif"))
+	{
+		strcpy(fileType, "image/gif");
+	}
+	else if (strstr(filename, ".png"))
+	{
+		strcpy(fileType, "image/png");
+	}
+	else if (strstr(filename, ".jpg"))
+	{
+		strcpy(fileType, "image/jpeg");
+	}
+	else
+	{
+		strcpy(fileType, "text/plain");
+	}
+}
+
+// 处理静态请求  
+void HandleStatic(SOCKET clientSocket, char * filename)
+{
+	// 直接返回服务器主页  
+	if (0 == strcmp(filename, "./www/"))
+	{
+		strcpy(filename, "./www/index.html");
+	}
+
+	// 检测请求文件的类型  
+	char fileType[20];
+	GetFileType(filename, fileType);
+	
+	FILE *fp = fopen(filename, "rb");
+
+	if (fp == 0)
+	{
+		// 如果找不到响应的数据就返回404状态码，并将其发送给客户端  
+		char response[] = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+		send(clientSocket, response, strlen(response), 0);
+	}
+	else
+	{
+		// 在文件目录中找响应的资源  
+		int file_size = 0;
+		char *content;
+		char *response;
+		fseek(fp, 0, SEEK_END);
+		file_size = ftell(fp);
+		fseek(fp, 0, SEEK_SET);
+		content = (char*)malloc(file_size + 1);
+		fread(content, file_size, 1, fp);
+		content[file_size] = 0;
+
+		response = (char*)malloc(file_size + 100);
+
+		// 发送请求headers到客户端  
+		sprintf(response, "HTTP/1.1 200 OK\r\n");
+		sprintf(response, "%sContent-Type: %s\r\n", response, fileType);
+		sprintf(response, "%sContent-Length: %d\r\n", response, file_size);
+		sprintf(response, "%sServer: Web Server\r\n\r\n", response);
+		//send(clientSocket, response, strlen(response), 0);
+
+		if (send(clientSocket, response, strlen(response), 0) == SOCKET_ERROR)
+		{
+			printf("Send headers failed!");
+			closesocket(clientSocket);
+		}
+		else
+		{
+			// 显示headers
+			printf(response);
+		}
+
+		// 发送请求body到客户端  
+		sprintf(response, "%s", content);
+		//send(clientSocket, response, strlen(response), NULL);
+
+		if (send(clientSocket, response, strlen(response), 0) == SOCKET_ERROR)
+		{
+			printf("Send body failed!");
+			closesocket(clientSocket);
+		}
+		else
+		{
+			// 显示body
+			printf(response);
+			printf("\r\n\r\n");
+		}
+
+		free(content);
+	}
+}
+
+int main()
+{
+	WSADATA              wsaData;
+	SOCKET               ListeningSocket;
+	SOCKET               NewConnection;
+	SOCKADDR_IN          ServerAddr;
+	SOCKADDR_IN          ClientAddr;
+	int                  Port = 81;
+	int					 ClientAddrLen;
+	char				 revBuff[1000];
+
+	// 初始化Windows Socket 2.0
+	if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0)
+	{
+		printf("sock init failed\n");
+		WSACleanup();
+		return -1;
+	}
+	printf("sock init succeed\n");
+
+	// 创建一个新的Socket来响应客户端的连接请求
+	ListeningSocket = socket(AF_INET, SOCK_STREAM, 0);
+	if (ListeningSocket == SOCKET_ERROR)
+	{
+		printf("sock create failed\n");
+		WSACleanup();
+		return -1;
+	}
+	printf("sock create successed\n");
+
+	// 填写服务器地址信息
+	// 端口为81
+	// IP地址为INADDR_ANY，htonl将IP地址转换为网络格式
+	ServerAddr.sin_family = AF_INET;// TCP/IP
+	ServerAddr.sin_port = htons(Port);
+	ServerAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	// 绑定监听端口
+	if (bind(ListeningSocket, (SOCKADDR *)&ServerAddr, sizeof(ServerAddr)) == SOCKET_ERROR)
+	{
+		printf("bind is failed\n");
+		closesocket(ListeningSocket);
+		WSACleanup();
+		return -1;
+	}
+	printf("bind is successsed\n");
+
+	// 开始监听，指定最大同时连接数为1024
+	if (listen(ListeningSocket, 1024) == SOCKET_ERROR)
+	{
+		printf("listen failed\n");
+		closesocket(ListeningSocket);
+		WSACleanup();
+		return -1;
+	}
+	printf("listening...\n");
+
+	ClientAddrLen = sizeof(sockaddr_in);
+	while (true)
+	{
+		// 接受新的连接
+		NewConnection = accept(ListeningSocket, (SOCKADDR *)&ClientAddr, &ClientAddrLen);
+		if (NewConnection == SOCKET_ERROR)
+		{
+			printf("sock create failed\n");
+			closesocket(NewConnection);
+			//return -1;
+			continue;
+		}
+		//printf("sock create successed\n");
+
+		// 接受请求
+		ZeroMemory(revBuff, 1000);
+		if (recv(NewConnection, revBuff, 1000, 0) == SOCKET_ERROR)
+		{
+			printf("recv failed\n");
+			closesocket(NewConnection);
+			//break;
+			continue;
+		}
+
+		// 显示请求
+		printf(revBuff);
+
+		// 响应浏览器
+		char temp[BUFSIZ];
+		char file[BUFSIZ];
+
+		strcpy(file, "./www");
+
+		if (sscanf(revBuff, "%s %s", temp, file + 5) != 2)
+			exit(1);
+
+		// 处理请求
+		HandleStatic(NewConnection, file);
+
+		// 关闭Socket
+		closesocket(NewConnection);
+	}
+
+	// 关闭监听Socket，然后退出应用程序  
+	closesocket(ListeningSocket);
+
+	// 释放Windows Socket 的相关资源
+	WSACleanup();
+
+	return 0;
+}
